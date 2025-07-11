eb3cac65ae780633d5ea906e8de48bb5325659fc
hain <hainofficialweb@gmail.com>
2025-07-11 16:09:11 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,82 +1,90 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-const MongoStore = require('connect-mongo');
-var userRouter = require('./routes/user');
-var hbs = require('express-handlebars');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+const MongoStore = require("connect-mongo");
+var userRouter = require("./routes/user");
+var hbs = require("express-handlebars");
 var app = express();
-var fileUpload = require('express-fileupload');
-var db = require('./config/connection');
-var session = require('express-session'); 
-require('dotenv').config();
-// CORS Middleware should be placed before route definitions 
-app.use(cors({
-  origin:['http://localhost:3000'], // Your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true  // Allow credentials (cookies) to be sent with requests
-}));
-// View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.engine('hbs', hbs.engine({
-  extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/',
-  helpers: {
-    lt: function (v1, v2) {
-      return v1 < v2;
+var fileUpload = require("express-fileupload");
+var db = require("./config/connection");
+var session = require("express-session");
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, 
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layout/",
+    partialsDir: __dirname + "/views/partials/",
+    helpers: {
+      lt: function (v1, v2) {
+        return v1 < v2;
+      },
+      eq: function (v1, v2) {
+        return v1 === v2;
+      },
+      multiply: function (v1, v2) {
+        return v1 * v2;
+      },
     },
-    eq: function (v1, v2) {
-      return v1 === v2;
-    },
-    multiply: function (v1, v2) {
-      return v1 * v2;
-    }
-  }
-}));
-app.use(logger('dev'));
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
-console.log('Environment:', app.get('env')); // Should print 'production' on Render
-// Session configuration (add this before your routes)
+console.log("Environment:", app.get("env")); 
+
 const sessionMiddleware = session({
-  secret: 'ajinajinshoppingsecretisajin',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: 'mongodb+srv://ajinrajeshhillten:ilzSIoQy0bzlzgF2@cluster0.prb1l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    collectionName: 'sessions',
+    mongoUrl: process.env.MONGO_URI,
+    collectionName: "sessions",
     ttl: 30 * 24 * 60 * 60, // Session TTL (30 day)
-    autoRemove: 'native',
-    touchAfter: 24 * 3600 // Time period in seconds between session updates
+    autoRemove: "native",
+    touchAfter: 24 * 3600, // Time period in seconds between session updates
   }),
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 dqys
-  }
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dqys
+  },
 });
-// Add this before your routes
-app.set('trust proxy', 1);
+
+app.set("trust proxy", 1);
 app.use(sessionMiddleware);
 // Database connection
 db.connect((err) => {
   if (err) {
-    console.log('Database not connected' + err);
+    console.log("Database not connected" + err);
   } else {
-    console.log('Database Connected ');
+    console.log("Database Connected ");
   }
 });
 // Route handling
-app.use('/', userRouter);
+app.use("/", userRouter);
 // Static public directory
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -85,9 +93,9 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 module.exports = app;
